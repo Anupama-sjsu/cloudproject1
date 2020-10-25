a2c75b26a56923e318137090d943f613de9421fe
Adarsh Kurudi <adarsh_32@yahoo.com>
2020-10-25 14:34:06 -0400

    Added express server.

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+
+const PORT = process.env.PORT || 4000;
+
+var app = express();
+app.set('port', PORT);
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('build'))
+app.listen(app.get('port'), function () {
+	console.log('Express server listening on port ' + app.get('port') + ' using nodemon.');
+});
